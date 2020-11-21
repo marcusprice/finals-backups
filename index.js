f84b2b6be144f3515f9796d69e04d37a5d891034
Marcus Price <marcusprice88@gmail.com>
2020-11-21 11:10:38 -0800

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//make a temporary folder to store backups - name is timestamped with today's date
+
+//back up the database and put the sql file in the temp folder
+
+//make a copy of the strapi public folder and move it into the temp folder
+
+//compress the temp folder
+
+//upload the compressed folder to drive
+
+//delete temp folder and compressed file
+
+//delete any drive files > 60 days old
