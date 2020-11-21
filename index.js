4e2899615245821b81566cd5deab76e3931cda34
Marcus Price <marcusprice88@gmail.com>
2020-11-21 12:28:07 -0800

    made folder creation and cleanup functions

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
+const fs = require('fs');
+const date = new Date();
+const today =
+  date.getMonth() + 1 + '-' + date.getDate() + '-' + date.getFullYear();
+let continueProgram = true;
+
+const newLogEntry = (today) => {
+  //set a new entry for the logs
+  fs.appendFileSync(__dirname + '/logs.txt', '\n\n' + today + ' Logs');
+};
+
+const logAndKill = (err) => {
+  fs.appendFileSync(__dirname + '/logs.txt', '\n' + err.toString());
+  process.exit(1);
+};
+
+const createNewDir = (today) => {
+  const path = __dirname + '/' + today + ' backup';
+
+  try {
+    fs.mkdirSync(path);
+  } catch (err) {
+    logAndKill(err);
+  }
+
+  return path;
+};
+
+const cleanUp = (dirPath) => {
+  try {
+    fs.rmdirSync(dirPath);
+  } catch (err) {
+    logAndKill(err);
+  }
+};
+
+const logSuccess = () => {
+  const message = 'Successful backup on ' + date.toUTCString();
+  fs.appendFileSync(__dirname + '/logs.txt', '\n' + message);
+};
+
+//set a new entry for the logs
+newLogEntry(today);
+
 //make a temporary folder to store backups - name is timestamped with today's date
+const dirPath = createNewDir(today);
 
 //back up the database and put the sql file in the temp folder
 
@@ -9,5 +54,8 @@
 //upload the compressed folder to drive
 
 //delete temp folder and compressed file
-
+cleanUp(dirPath);
 //delete any drive files > 60 days old
+
+//log success
+logSuccess();
